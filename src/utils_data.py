ff39661e8fd346a9f86a3de65826f24de79141bf
Natalia <nasanjo@gmail.com>
2025-10-27 19:07:05 +0100

    add yahoo and marketstack folders to gitignore

---
+++ src/utils_data.py
@@ -0,0 +1,36 @@
+# utils.py
+from datetime import datetime
+from typing import List, Dict, Any
+from data_classes import DailyPrice
+
+
+def standard_data(data: List[Dict[str, Any]], source: str) -> List[Dict[str, Any]]:
+    """Convierte los datos crudos en formato estandarizado."""
+    return [{
+        "date": entry["date"],
+        "open": float(entry["open"]),
+        "high": float(entry["high"]),
+        "low": float(entry["low"]),
+        "close": float(entry["close"]),
+        "adj_close": float(entry["adj_close"]),
+        "volume": int(entry["volume"]),
+        "source": source
+    } for entry in data]
+
+def convert_to_dailyprice(time_series: List[Dict[str, Any]]) -> List[DailyPrice]:
+    """Convierte los datos estandarizados en objetos DailyPrice."""
+    list_price = []
+    for data in time_series:
+        fecha = datetime.strptime(data["date"], "%Y-%m-%d").date()
+        list_price.append(
+            DailyPrice(
+                date=fecha,
+                open=data["open"],
+                high=data["high"],
+                low=data["low"],
+                close=data["close"],
+                adj_close=data["adj_close"],
+                volume=data["volume"]
+            )
+        )
+    return sorted(list_price, key=lambda p: p.date)
